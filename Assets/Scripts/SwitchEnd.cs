d523e547938ee0061aea28a99281974f92adf461
jackmelcher <Student@aii.stu>
2018-09-29 17:20:38 -0700

    added most of core mechanics

---
+++ Assets/Scripts/SwitchEnd.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchEnd : MonoBehaviour {
+
+	public bool isSwitched = false;
+	public bool canDeactivate = false;
+
+	public GameObject[] prefabs;
+	private int players = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(col.gameObject.tag == "Player"){
+			players++;
+			if(players >= 2){
+				isSwitched = true;
+				canDeactivate = false;
+				foreach(GameObject prefab in prefabs){
+					Debug.Log(prefab.name);
+					prefab.GetComponent<Activate>().Activation();
+				}
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider col){
+		if(col.gameObject.tag == "Player"){
+			players--;
+			if(canDeactivate){
+				if(players < 2){
+					isSwitched = false;
+					foreach(GameObject prefab in prefabs){
+						prefab.GetComponent<Activate>().Deactivation();
+					}
+				}
+			}
+		}
+		
+	}
+}
---
+++ Assets/Scripts/TrapSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSpawner : Activate {
+
+	public GameObject prefab;
+	public bool Repeats = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Activating){
+			Debug.Log("Fire Arrows");
+			if(Repeats){
+				InvokeRepeating("FireProjectile",0f,1f);
+			}
+			else{
+				Invoke("FireProjectile",0f);
+			}
+			Activating = false;
+		}
+
+		if(Deactivating){
+			Debug.Log("Stop Firing Arrows");
+			CancelInvoke("FireProjectile");
+			Deactivating = false;
+		}
+	}
+
+	void FireProjectile(){
+		Instantiate(prefab, transform.position,transform.rotation);
+	}
+}
